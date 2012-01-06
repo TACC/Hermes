ecd8414730657935ac4224523430e29cf49f849c
Robert McLay <mclay@zaniahgroup.com>
2012-01-05 20:52:21 -0600

    H5MGF diff tool

---
+++ H5MGFdiff/CmdLineOptions.lua
@@ -0,0 +1,40 @@
+-- $Id: CmdLineOptions.lua 302 2009-02-04 23:56:32Z mclay $ --
+
+CmdLineOptions = BaseTask:new()
+
+function CmdLineOptions:execute(myTable)
+
+   local usage         = "H5MGFdiff -r resultFn -f xdrdiff.lua  xdr1 xdr2"
+   local Optiks        = require("Optiks")
+   local cmdlineParser = Optiks:new{usage=usage, error = Error}
+
+   cmdlineParser:add_option{ 
+      name   = {'-v','--verbose'},
+      dest   = 'verbosityLevel',
+      action = 'count'
+   }
+
+   cmdlineParser:add_option{ 
+      name   = {'-f','--file'},
+      dest   = 'tdesc',
+      action = 'store'
+   }
+   cmdlineParser:add_option{ 
+      name   = {'-r', '--resultfile'},
+      dest   = 'resultFn',
+      action = 'store'
+   }
+
+   local optionTbl, pargs = cmdlineParser:parse(arg)
+   local masterTbl        = masterTbl()
+
+   for v in pairs(optionTbl) do
+      masterTbl[v] = optionTbl[v]
+   end
+
+   masterTbl.pargs = pargs
+
+   if (#masterTbl.pargs < 2) then
+      Error("Usage: ",usage)
+   end
+end
---
+++ H5MGFdiff/Wrapper.lua
@@ -0,0 +1,38 @@
+-- $Id: Wrapper.lua 327 2009-08-24 20:03:46Z eijkhout $ --
+require("serializeTbl")
+require("fileOps")
+Wrapper = BaseTask:new()
+
+function Wrapper:execute(myTable)
+   local masterTbl = masterTbl()
+   
+   assert(loadfile(masterTbl.tdesc))()
+   
+   
+   local cmdA = {}
+   cmdA[#cmdA + 1] = "compMGF"
+   cmdA[#cmdA + 1] = "-t"
+   cmdA[#cmdA + 1] = self:prep(masterTbl.pargs[1])
+   cmdA[#cmdA + 1] = "-g"
+   cmdA[#cmdA + 1] = self:prep(masterTbl.pargs[2])
+   cmdA[#cmdA + 1] = "-r"
+   cmdA[#cmdA + 1] = masterTbl.resultFn
+
+   for i,v in ipairs(testdescript.tolerances.vars) do
+      cmdA[#cmdA + 1] = v.name .. " " .. v.rel .. " " .. v.abs
+   end
+ 
+   local cmdline = table.concat(cmdA," ")
+   print ("cmdline:",cmdline)
+
+   masterTbl.status = os.execute(cmdline .. " > H5MGFdiff.log 2>&1")
+
+end
+
+function Wrapper:prep(fn)
+   local dir      = dirname(fn)
+   local bfn      = barefilename(fn)
+   local i,j, fn1 = bfn:find("(.*)_%d_soln.h5")
+   if (i) then bfn = fn1 end
+   return pathJoin(dir,bfn)
+end
