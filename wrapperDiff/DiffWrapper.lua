bb0f7dcfaffb5d1bb5917bafe065ca9dc96e3be5
Robert McLay <mclay@zaniahgroup.com>
2012-09-08 23:52:49 -0500

    support for lmod version in err.txt files

--- wrapperDiff/DiffWrapper.lua
+++ wrapperDiff/DiffWrapper.lua
@@ -1,22 +1,41 @@
 -- $Id: Wrapper.lua 194 2008-06-25 21:43:50Z mclay $ --
 
 require("serializeTbl")
+require("fileOps")
 
 myTbl = {}
 
+concatTbl   = table.concat
 DiffWrapper = BaseTask:new()
 
 function DiffWrapper:execute(myTable)
    local masterTbl = masterTbl()
-   local cmdA      = {}
+
+   local origA      = { masterTbl.pargs[1], masterTbl.pargs[2] } 
+   local modA       = { removeExt(barefilename(origA[1])) .. ".left",
+                        removeExt(barefilename(origA[2])) .. ".right",}
+   local cmdA       = {}
+   local cmdLine
+
+   for i = 1,2 do
+      cmdA[#cmdA + 1 ] = "sed -e '/Modules based on Lua: Version/d' <"
+      cmdA[#cmdA + 1 ] = origA[i]
+      cmdA[#cmdA + 1 ] = ">"
+      cmdA[#cmdA + 1 ] = modA[i]
+
+      cmdLine = concatTbl(cmdA," ")
+      print (cmdLine)
+      os.execute(cmdLine)
+      cmdA = {}
+   end
 
    cmdA[#cmdA + 1] = "diff -c "
 
-   cmdA[#cmdA + 1] = masterTbl.pargs[1]
-   cmdA[#cmdA + 1] = masterTbl.pargs[2]
+   cmdA[#cmdA + 1] = modA[1]
+   cmdA[#cmdA + 1] = modA[2]
    cmdA[#cmdA + 1] = "> diff.log 2>&1"
 
-   local cmdline = table.concat(cmdA," ")
+   local cmdline = concatTbl(cmdA," ")
 
    print (cmdline)
    local status     = os.execute(cmdline)
@@ -34,6 +53,8 @@ function DiffWrapper:execute(myTable)
       result = "passed"
    end
    os.remove("diff.log")
+   os.remove(modA[1])
+   os.remove(modA[2])
    myTbl[#myTbl+1] = { result=result, program="wrapperDiff"}
    serializeTbl{name="myTbl", value=myTbl, fn=resultFn, indent=true}
 end
