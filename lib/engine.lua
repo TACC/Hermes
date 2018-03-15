507d14ca1102c0094ba08aa181286a7808c8e58c
Robert McLay <mclay@zaniahgroup.com>
2018-03-15 12:15:16 -0500

    remove setfenv()

--- lib/engine.lua
+++ lib/engine.lua
@@ -1,27 +1,7 @@
 -- -*- lua -*-
 
-require("strict")
-require("declare")
-
-if (not isDefined("setfenv")) then -- Lua 5.2
-  -- based on http://lua-users.org/lists/lua-l/2010-06/msg00314.html
-  -- this assumes f is a function
-  local function findenv(f)
-    local level = 1
-    repeat
-      local name, value = debug.getupvalue(f, level)
-      if name == '_ENV' then return level, value end
-      level = level + 1
-    until name == nil
-    return nil end
-  getfenv = function (f) return(select(2, findenv(f)) or _G) end
-  setfenv = function (f, t)
-    local level = findenv(f)
-    if level then debug.setupvalue(f, level, t) end
-    return f end
-end
-
 _DEBUG      = false
+require("strict")
 local dbg   = require("Dbg"):dbg()
 local posix = require("posix")
 require("fileOps")
